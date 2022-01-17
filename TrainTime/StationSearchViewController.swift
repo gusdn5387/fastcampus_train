10cb40b42ad13d6ee59f9c1e632b072bc1382ddb
박현우 <phw@baghyeon-uui-MacBookPro-2.local>
2022-01-18 01:42:15 +0900

    지하철역 검색화면 구현하기 완

--- TrainTime/StationSearchViewController.swift
+++ TrainTime/StationSearchViewController.swift
@@ -9,18 +9,63 @@ import UIKit
 import SnapKit
 
 class StationSearchViewController: UIViewController {
-
+    private var numberOfCell: Int = 0
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.isHidden = true
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setNavigationItems()
+        setTableViewLayout()
+    }
+    
+    private func setNavigationItems() {
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "지하철 도착 정보"
         
         let searchController = UISearchController()
         searchController.searchBar.placeholder = "지하철역을 입력해주세요."
         searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
         
         navigationItem.searchController = searchController
     }
+    
+    private func setTableViewLayout() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
 }
 
+extension StationSearchViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        numberOfCell = 10
+        tableView.isHidden = false
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        tableView.isHidden = true
+    }
+}
+
+extension StationSearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfCell
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "\(indexPath.item)"
+        
+        return cell
+    }
+}
